da18e58ed32310b0552166a2bcdf074bacdcd1d8
gustavodscruz <gustavodiasdsc@gmail.com>
2023-06-12 12:00:17 -0300

    first commit

---
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.example.chatapp
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.chatapp.ui.theme.ChatAppTheme
+import com.example.chatapp.ui.theme.SampleData
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ChatAppTheme {
+                Conversation(SampleData.conversationSampleLists)
+
+            }
+        }
+    }
+
+    data class conversationSample(val author:String, val body:String)
+
+    @Composable
+    fun MessageCard(msg: conversationSample){
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.profile_picture),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+            )
+            
+            Spacer(modifier = Modifier.width(8.dp))
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            )
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.titleMedium
+
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp, color = surfaceColor, modifier = Modifier.animateContentSize().padding(1.dp)) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.bodyMedium,
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                    )
+
+                }
+
+            }
+
+        }
+
+    }
+
+    @Composable
+    fun Conversation(conversationSamples : List<conversationSample>){
+        LazyColumn{
+            items(conversationSamples){
+                    message -> MessageCard(message)
+            }
+        }
+    }
+
+    @Preview("Ligth Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+    @Composable
+    fun PreviewMessageCard(){
+        ChatAppTheme() {
+            Conversation(SampleData.conversationSampleLists)
+        }
+        }
+}
---
+++ app/src/main/java/com/example/chatapp/ui/theme/SampleData.kt
@@ -0,0 +1,17 @@
+package com.example.chatapp.ui.theme
+
+import com.example.chatapp.MainActivity
+
+object SampleData{
+    val conversationSampleLists = listOf(
+        MainActivity.conversationSample("Peralta", "Sou um lindo cachorrinho!"),
+        MainActivity.conversationSample("Peralta", "Sou um lindo cachorrinho! E muito bonitinho!"),
+        MainActivity.conversationSample("Peralta", "Sou um lindo cachorrinho!"),
+        MainActivity.conversationSample("Peralta", "Sou um lindo cachorrinho!  E muito bonitinho!"),
+        MainActivity.conversationSample("Peralta", "Sou um lindo cachorrinho!"),
+        MainActivity.conversationSample("Peralta", "Sou um lindo cachorrinho! E muito bonitinho!"),
+        MainActivity.conversationSample("Peralta", "Sou um lindo cachorrinho!"),
+        MainActivity.conversationSample("Peralta", "Sou um lindo cachorrinho! E muito bonitinho!"),
+        MainActivity.conversationSample("Peralta", "Sou um lindo cachorrinho!"),
+    )
+}
